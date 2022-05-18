57f83f0eb159eb690b93abd17df7f7e3c2875704
nxsense <99249892+nxsense@users.noreply.github.com>
2022-05-18 14:09:38 +0300

    Add files via upload

---
+++ NodeOverrided.java
@@ -0,0 +1,13 @@
+public class NodeOverrided<K, V> {
+    V value;
+    K key;
+    NodeOverrided<K, V> next;
+
+    public NodeOverrided( K key, V value, NodeOverrided<K, V> next) {
+        this.value = value;
+        this.key = key;
+        this.next = next;
+    }
+    public NodeOverrided() {
+    }
+}
